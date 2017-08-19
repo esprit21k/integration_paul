bccff1bad7c2c98e836339b954bb1be9e0ea9021
Jason Lee <jason@mytrum.com>
2017-08-18 17:03:15 -0700

    adding semantic-ui-react dependency

--- src/main/java/com/trumpia/WebSecurityConfig.java
+++ src/main/java/com/trumpia/WebSecurityConfig.java
@@ -13,10 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.trumpia.auth.JWTAuthenticationFilter;
 import com.trumpia.auth.JWTAuthorizationFilter;
-import com.trumpia.auth.SecurityHandler;
-import com.trumpia.data.UserRepository;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
--- src/main/java/com/trumpia/views/LoginController.java
+++ src/main/java/com/trumpia/views/LoginController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,5 +62,5 @@ public class LoginController {
 		response.setData(JSONUtils.getNewObjectNode().put("email", user.getEmail()));
 		return response.getJSONResponse();
     }
-
+	
 }
